03363301485dd61ebd117a87af4cf22ff1371c85
Minecraft-Cake <96643025+Minecraft-Cake@users.noreply.github.com>
2022-11-06 09:15:50 -1000

    I forgor

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send("Hello lol")
+    res.sendFile(path.join(__dirname, '/html/index.html'));
+
 })
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`)
